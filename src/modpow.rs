ad7e1f1dccc9357a9a46e2c53570bb7b8112f09d
LambdaXymox <lambda.xymox@gmail.com>
2016-06-04 20:21:01 -0500

    Code cleanup.

--- src/modpow.rs
+++ src/modpow.rs
@@ -37,36 +37,3 @@ impl<T> ModPow for T where T: Clone + Num + Integer {
         result
     }
 }
-
-/*
-impl<T> ModPow for T where T: Copy + Clone + Num + Integer + Shr<usize> + ShrAssign<usize> {
-    type IntType = T;
-
-    fn mod_pow(base: &T, exponent: &T, modulus: &T) -> T {
-        let zero: T = Zero::zero();
-
-        assert!(*modulus != zero);
-
-        let one: T = One::one();
-        let two: T = <T as One>::one() + <T as One>::one();
-
-        if *modulus == one {
-            return zero;
-        }
-
-        let mut result: T = One::one();
-        let mut modded_base: T = base.mod_floor(modulus);
-        let mut divided_exponent: T = exponent.clone();
-        
-        while divided_exponent > zero {
-            if divided_exponent.mod_floor(&two) == one {
-                result = (result * &modded_base).mod_floor(modulus);
-            }
-            divided_exponent >>= 2;
-            modded_base = (&modded_base * &modded_base).mod_floor(modulus);
-        }
-
-        result
-    }
-}
-*/
