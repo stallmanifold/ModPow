62824cd3917901623e08619908872fa83224ba66
LambdaXymox <lambda.xymox@gmail.com>
2016-07-10 20:44:45 -0500

    Merge unit tests into source files.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 pub use modexp::ModExp;
 pub use modmult::ModMult;
 pub use extended_gcd::ExtendedGcd;
-//pub use modinv::ModInv;
+pub use modinv::ModInv;
 
 
 extern crate num;
@@ -12,16 +12,3 @@ pub mod modexp;
 pub mod modmult;
 pub mod extended_gcd;
 pub mod modinv;
-
-
-#[cfg(test)]
-mod modexp_tests;
-
-#[cfg(all(test))]
-mod modexp_bench;
-
-#[cfg(test)]
-mod extended_gcd_tests;
-
-#[cfg(test)]
-mod modinv_tests;
